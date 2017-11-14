c2fe01cba5ba3f12f7a1c6d847cc2b4aad77403b
Blake Zimmerman <blake.zimmerman@jet.com>
2017-11-14 13:34:32 -0500

    Add a little bit of styles

---
+++ src/app/app.styles.ts
@@ -0,0 +1,15 @@
+import { CSSProperties } from 'react';
+
+export const counter: CSSProperties = {
+  padding: '1rem'
+};
+
+export const data: CSSProperties = {
+  padding: '1rem'
+};
+
+export const links: CSSProperties = {
+  padding: '1rem',
+  display: 'flex',
+  flexDirection: 'column'
+};
--- src/app/app.tsx
+++ src/app/app.tsx
@@ -9,6 +9,7 @@ import PageOne from './pages/pageOne';
 import PageTwo from './pages/pageTwo';
 import NotFound from './pages/404';
 import Link from 'redux-first-router-link';
+import * as appStyles from './app.styles';
 
 interface Props {
   location: LocationState,
@@ -25,19 +26,24 @@ const App = (props: Props) => {
 
   const ActionsDemo = () => (
     <div>
-      <div>
+      <div style={appStyles.counter}>
         <div>Count by twos!</div>
         <button onClick={incrementByTwo}>+</button>
         <div>Current Value: {props.app.counter}</div>
         <button onClick={decrementByTwo}>-</button>
       </div>
-      <div>
+      <div style={appStyles.data}>
         <button onClick={fetchData}>Fetch Data</button>
         <div>
           {'Current Data: '}
-          {props.app.data.result ? props.app.data.result.toString() : 'None'}
+          {props.app.data.pending ? 'Pending...' :
+           props.app.data.result ? props.app.data.result.toString() : 'None'
+          }
         </div>
+      </div>
+      <div style={appStyles.links}>
         <Link to='/page-one'>Go to Page One</Link>
+        <Link to='/page-two'>Go to Page Two</Link>
       </div>
 
     </div>
